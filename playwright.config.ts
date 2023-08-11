1c88643491c82e84fe67a88969fcf4e827185b61
Jonas Bulik <jonas@bulik.dev>
2023-08-11 14:57:41 +0200

    add first visual tests and include mobile

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,31 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'pnpm run build && pnpm run preview',
 		port: 4173
 	},
+	projects: [
+		{
+			name: 'chromium',
+			use: {
+				...devices['Desktop Chrome']
+			}
+		},
+		{
+			name: 'firefox',
+			use: {
+				...devices['Desktop Firefox']
+			}
+		},
+		{
+			name: 'Mobile Chrome',
+			use: {
+				...devices['Pixel 5']
+			}
+		}
+	],
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/
 };
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,17 +1,35 @@
 import { expect, test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-	page.goto('/');
+	await page.goto('/');
 });
 
 test('index page screenshot', async ({ page }) => {
 	await expect(page).toHaveScreenshot();
 });
-test('index page has navigation to home', async ({ page }) => {
+test('index page has navigation', async ({ page }) => {
 	await expect(page.getByRole('navigation')).toBeVisible();
 });
 
-test('clicking Home navigates to Home', async ({ page }) => {
-	await page.getByRole('link', { name: 'Home' }).click();
-	await expect(page).toHaveURL('/');
+for (const subPage of [
+	{ name: 'Home', url: '/' }
+	// TODO: Add tests for new sub pages
+]) {
+	test(`clicking '${subPage.name}' navigates to ${subPage.url}`, async ({ page, isMobile }) => {
+		if (isMobile) {
+			await page.getByRole('button', { name: 'Open main menu' }).click();
+		}
+		await page.getByRole('link', { name: subPage.name }).click();
+		await expect(page).toHaveURL(subPage.url);
+	});
+}
+
+test.describe('Mobile only', () => {
+	test.skip(({ isMobile }) => !isMobile, 'Mobile only');
+
+	test('on mobile opens main menu', async ({ page }) => {
+		await page.getByRole('button', { name: 'Open main menu' }).click();
+		await expect(page).toHaveScreenshot();
+		await expect(page.getByRole('link', { name: 'Home' })).toBeVisible();
+	});
 });
