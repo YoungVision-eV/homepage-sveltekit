2c2f96cb46af6edd307564d6f19f6e446873ba5c
Jonas Bulik <jonas@bulik.dev>
2023-08-11 12:20:17 +0200

    add our colors to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,22 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const typography = require('@tailwindcss/typography');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
+		colors: {
+			black: colors.black,
+			white: colors.white,
+			'dark-green': '#6A845E',
+			'light-green': '#BBCEA8',
+			gray: '#F9F8F6',
+			orange: '#E97D2B',
+			yellow: '#F0EC57',
+			pink: '#EA6C92'
+		},
 		extend: {}
 	},
 
