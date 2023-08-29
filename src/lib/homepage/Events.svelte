c2988aad7cabfcfa06c9f2a86c1f516cc45b6e74
Jonas Bulik <jonas@bulik.dev>
2023-08-29 20:07:45 +0200

    Add events section to index page

---
+++ src/lib/homepage/Events.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Button from '$lib/components/Button.svelte';
+</script>
+
+<div class="w-full bg-cover pt-16 pb-14 px-5">
+	<h1 class="text-3xl font-bold font-serif">Unsere Events</h1>
+	<p class="mt-4 text-xl">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua.
+	</p>
+	<Button class="mt-14" text="Follow us on Telegram" inverted={true} />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,12 @@
 	import Navbar from '$lib/homepage/Navbar.svelte';
 	import Hero from '$lib/homepage/Hero.svelte';
 	import ReadMore from '$lib/homepage/ReadMore.svelte';
+	import Events from '$lib/homepage/Events.svelte';
 </script>
 
 <Navbar />
 <main>
 	<Hero />
 	<ReadMore />
+	<Events />
 </main>
