9a8ff435955927f9585877f99146aca001a3dc04
Jonas Bulik <jonas@bulik.dev>
2023-09-01 19:10:43 +0200

    Implement calendar for desktop layout

--- src/lib/homepage/Calendar.svelte
+++ src/lib/homepage/Calendar.svelte
@@ -2,40 +2,85 @@
 	import calendarCover from '$lib/assets/calendar-cover.jpeg';
 </script>
 
-<div class="w-full">
-	<img src={calendarCover} alt="Leute sitzen am Tisch" class="w-full rounded-t-2xl" />
-	<div class="py-3 pl-7 pr-6 flex items-center justify-between gap-x-12">
-		<div class="text-center flex-none">
-			<div class="text-5xl font-bold font-serif">24</div>
-			<div>August</div>
-		</div>
-		<div class="flex-auto">
-			<h2 class="text-xl font-bold line-clamp-2">Our Next Event: YV Gathering</h2>
-			<p class="leading-6 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+<div class="w-full md:px-16 md:flex">
+	<div
+		class="w-full md:relative h-auto md:w-[42rem] md:h-[27rem] md:rounded-r-none md:rounded-l-2xl"
+	>
+		<img
+			src={calendarCover}
+			alt="Leute sitzen am Tisch"
+			class="object-cover h-full w-full rounded-t-2xl"
+		/>
+		<div class="hidden md:flex flex-col gap-y-1 justify-center absolute right-[8.3rem] inset-y-0">
+			<svg
+				class="w-3.5 h-3.5 text-white"
+				fill="currentColor"
+				viewBox="0 0 100 100"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<circle cx="50" cy="50" r="50" />
+			</svg>
+			<svg
+				class="w-3.5 h-3.5 text-soft-yellow"
+				fill="currentColor"
+				viewBox="0 0 100 100"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<circle cx="50" cy="50" r="50" />
+			</svg>
+			<svg
+				class="w-3.5 h-3.5 text-soft-yellow"
+				fill="currentColor"
+				viewBox="0 0 100 100"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<circle cx="50" cy="50" r="50" />
+			</svg>
 		</div>
 	</div>
-	<div
-		class="py-3 pl-7 pr-6 flex items-center justify-between gap-x-12 bg-soft-yellow text-dark-green"
-	>
-		<div class="text-center flex-none">
-			<div class="text-5xl font-bold font-serif">12</div>
-			<div>Juni</div>
+	<!-- TODO: relative needed here for correct z-ordering, is there a better way? -->
+	<div class="md:flex relative flex-col grow md:-ml-28">
+		<div
+			class="py-3 pl-7 pr-6 flex items-center justify-between grow gap-x-12 md:bg-white md:rounded-2xl md:rounded-br-none md:-ml-4"
+		>
+			<div class="text-center flex-none">
+				<div class="text-5xl font-bold font-serif">24</div>
+				<div>August</div>
+			</div>
+			<div class="flex-auto">
+				<h2 class="text-xl font-bold line-clamp-2">Our Next Event: YV Gathering</h2>
+				<p class="leading-6 text-justify">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				</p>
+			</div>
 		</div>
-		<div class="flex-auto">
-			<h2 class="text-xl font-bold line-clamp-2">Our Last Event: Playfulness Workshop</h2>
-			<p class="leading-6 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+		<div
+			class="py-3 pl-7 pr-6 flex items-center justify-between grow gap-x-12 bg-soft-yellow text-dark-green"
+		>
+			<div class="text-center flex-none md:text-white">
+				<div class="text-5xl font-bold font-serif">12</div>
+				<div>Juni</div>
+			</div>
+			<div class="flex-auto">
+				<h2 class="text-xl font-bold line-clamp-2">Our Last Event: Playfulness Workshop</h2>
+				<p class="leading-6 text-justify">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				</p>
+			</div>
 		</div>
-	</div>
-	<div
-		class="py-3 pl-7 pr-6 flex items-center justify-between gap-x-12 bg-soft-yellow text-dark-green"
-	>
-		<div class="text-center flex-none">
-			<div class="text-5xl font-bold font-serif">15</div>
-			<div>Oktober</div>
-		</div>
-		<div class="flex-auto">
-			<h2 class="text-xl font-bold line-clamp-2">Workshop with our Partner: XX</h2>
-			<p class="leading-6 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+		<div
+			class="py-3 pl-7 pr-6 flex items-center justify-between grow gap-x-12 bg-soft-yellow text-dark-green"
+		>
+			<div class="text-center flex-none md:text-white">
+				<div class="text-5xl font-bold font-serif">15</div>
+				<div>Oktober</div>
+			</div>
+			<div class="flex-auto">
+				<h2 class="text-xl font-bold line-clamp-2">Workshop with our Partner: XX</h2>
+				<p class="leading-6 text-justify">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				</p>
+			</div>
 		</div>
 	</div>
 </div>
--- src/lib/homepage/Events.svelte
+++ src/lib/homepage/Events.svelte
@@ -4,13 +4,23 @@
 </script>
 
 <section class="w-full bg-cover pt-16">
-	<div class="px-5 pb-14">
-		<h1 class="text-3xl font-bold font-serif">Unsere Events</h1>
-		<p class="mt-4 text-xl">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-			labore et dolore magna aliqua.
-		</p>
-		<Button class="mt-14" text="Follow us on Telegram" color="black" />
+	<div class="px-5 md:px-16">
+		<h1 class="text-3xl md:text-4xl font-bold font-serif">Unsere Events</h1>
 	</div>
-	<Calendar />
+	<div class="md:flex flex-col-reverse md:mt-14">
+		<div class="px-5 md:px-16 pb-14 md:mt-14">
+			<div class="mt-4 md:flex gap-x-8 justify-between">
+				<p class="text-xl">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+					ut labore et dolore magna aliqua.
+				</p>
+				<p class="text-xl hidden md:block">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+					ut labore et dolore magna aliqua ut enim ad.
+				</p>
+			</div>
+			<Button class="mt-14" text="Follow us on Telegram" color="black" />
+		</div>
+		<Calendar />
+	</div>
 </section>
