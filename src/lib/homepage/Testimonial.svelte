d80d384c362cc6bbc5255f5ebc82c7663d1fd1e9
Jonas Bulik <jonas@bulik.dev>
2023-09-01 20:11:13 +0200

    Implement testimonial desktop layout

--- src/lib/homepage/Testimonial.svelte
+++ src/lib/homepage/Testimonial.svelte
@@ -6,22 +6,27 @@
 
 <!-- TODO: add Background -->
 
-<section class="w-full pt-24 pb-12 px-5">
+<section class="relative w-full pt-24 pb-12 px-5 md:px-16">
 	<h1 class="text-3xl font-bold font-serif">Von YoungVision Leuten</h1>
-	<figure>
-		<div class="w-full mt-3 flex justify-between items-center">
-			<img src={ArrowLeft} alt="" class="w-6 h-6 -ml-3" />
-			<img src={LenaProfile} alt="" class="w-24 h-24 rounded-full object-cover" />
-			<img src={ArrowRight} alt="" class="w-6 h-6 -mr-3" />
+	<div class="mt-3 md:mt-16 md:flex gap-x-28 justify-between items-center md:items-center">
+		<div class="w-full md:mt-0 flex justify-center items-center">
+			<img src={ArrowLeft} alt="" class="w-6 h-6 absolute left-2 md:left-8" />
+			<img src={ArrowRight} alt="" class="w-6 h-6 absolute right-2 md:right-8" />
+			<img src={LenaProfile} alt="" class="w-24 h-24 md:w-96 md:h-96 rounded-full object-cover" />
 		</div>
 
-		<blockquote class="mt-2.5 leading-6">
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqua ut enim ad.
-			</p>
-		</blockquote>
+		<div class="md:pb-4">
+			<blockquote class="mt-2.5 leading-6 md:text-xl">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+					ut labore et dolore magna aliqua ut enim ad.
+				</p>
+				<p class="hidden md:block mt-8">
+					Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad.
+				</p>
+			</blockquote>
 
-		<figcaption class="mt-2 italic">Name, Age</figcaption>
-	</figure>
+			<p class="mt-2 md:mt-8 italic">Name, Age</p>
+		</div>
+	</div>
 </section>
