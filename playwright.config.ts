1884059bf04a991bd18d17a1192e3e2f5863874e
Jonas Bulik <jonas@bulik.dev>
2023-09-02 14:38:16 +0200

    add github annotations for playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,8 @@ const config: PlaywrightTestConfig = {
 		}
 	],
 	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+	reporter: process.env.CI ? 'github' : 'list'
 };
 
 export default config;
