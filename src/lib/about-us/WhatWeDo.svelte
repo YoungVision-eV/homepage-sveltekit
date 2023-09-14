e7abaf86c9468d0a34afdb831f83c3eba9daa399
Jonas Bulik <jonas@bulik.dev>
2023-09-14 21:51:06 +0200

    Add what we do to about us page

---
+++ src/lib/about-us/WhatWeDo.svelte
@@ -0,0 +1,36 @@
+<section class="w-full pt-24 pb-16 px-5">
+	<h1 class="text-center text-3xl font-bold font-serif">Was wir tun</h1>
+	<h2 class="mt-14 text-2xl font-bold font-serif">Main exercises</h2>
+	<p class="mt-6 font-sans">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua ut enim ad.
+	</p>
+	<p class="font-sans mt-3">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua ut enim ad.
+	</p>
+	<p class="font-sans mt-3">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua ut enim ad.
+	</p>
+	<div class="mt-16">
+		<div class="bg-gray-300 w-full h-auto aspect-square rounded-full" />
+		<h3 class="mt-5 text-center text-xl font-sans">Lorem Ipsum</h3>
+		<p class="mt-3 text-sm font-sans">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua ut enim ad.
+		</p>
+	</div>
+	<div class="mt-12">
+		<div class="bg-gray-300 w-full h-auto aspect-square rounded-full" />
+		<h3 class="mt-5 text-center text-xl font-sans">Lorem Ipsum</h3>
+		<p class="mt-3 text-sm font-sans">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua ut enim ad.
+		</p>
+	</div>
+	<div class="mt-16 w-full flex flex-col justify-center">
+		<button class="text-sm">See More</button>
+		<div class="mt-6 w-full border-t border-gray-300" />
+	</div>
+</section>
--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import TeamMembers from '$lib/about-us/TeamMembers.svelte';
+	import WhatWeDo from '$lib/about-us/WhatWeDo.svelte';
 </script>
 
 <TeamMembers />
+<WhatWeDo />
