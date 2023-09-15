3cf79d2b97228e0265b5f07594f80faa95028668
Jonas Bulik <jonas@bulik.dev>
2023-09-15 16:36:10 +0200

    Add try at home section

---
+++ src/lib/about-us/TryThisAtHome.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Button from '$lib/components/Button.svelte';
+</script>
+
+<section class="w-full bg-yellow px-5 py-10">
+	<h1 class="text-2xl font-bold font-serif text-center">Try this with your friends!</h1>
+	<p class="text-sm mt-6">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua ut enim ad.
+	</p>
+	<Button class="mt-14 mx-auto" color="black" text="Call to action" />
+</section>
--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import TeamMembers from '$lib/about-us/TeamMembers.svelte';
+	import TryThisAtHome from '$lib/about-us/TryThisAtHome.svelte';
 	import WhatWeDo from '$lib/about-us/WhatWeDo.svelte';
 </script>
 
 <TeamMembers />
 <WhatWeDo />
+<TryThisAtHome />
