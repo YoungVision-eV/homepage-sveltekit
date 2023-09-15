94f398d1a14995533fd64989cb0ee8db5220ff76
Jonas Bulik <jonas@bulik.dev>
2023-09-15 16:56:43 +0200

    Add workshop leader section

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -2,8 +2,10 @@
 	import TeamMembers from '$lib/about-us/TeamMembers.svelte';
 	import TryThisAtHome from '$lib/about-us/TryThisAtHome.svelte';
 	import WhatWeDo from '$lib/about-us/WhatWeDo.svelte';
+	import WorkshopLeaders from '$lib/about-us/WorkshopLeaders.svelte';
 </script>
 
 <TeamMembers />
 <WhatWeDo />
 <TryThisAtHome />
+<WorkshopLeaders />
