d19fd55434a9b5ac8d08d3f5c6dae4eeac1aa00b
Jonas Bulik <jonas@bulik.dev>
2023-10-05 17:39:23 +0200

    Add history section to about-us page

---
+++ src/lib/about-us/History.svelte
@@ -0,0 +1,16 @@
+<script>
+	import LunaPortrait from '$lib/assets/luna-about-us-history.jpeg';
+</script>
+
+<section class="w-full bg-light-green px-5 pt-10 pb-16 mb-28 rounded-2xl">
+	<h1 class="text-2xl font-bold font-serif">Die Geschichte</h1>
+	<img src={LunaPortrait} alt="" class="w-full mt-10 rounded-2xl object-cover h-96" />
+	<p class="mt-8">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua ut enim ad.
+	</p>
+	<p class="mt-8">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua ut enim ad.
+	</p>
+</section>
