443228f1f94d4437514aa5b94a31e8225ee955cd
Jonas Bulik <jonas@bulik.dev>
2023-10-05 18:05:24 +0200

    Add small expand icon for all about-us sections

---
+++ src/lib/components/ReadMoreExpandButton.svelte
@@ -0,0 +1,26 @@
+<script>
+	let className = '';
+	export { className as class };
+</script>
+
+<div class="w-full flex flex-col justify-center items-center {className}">
+	<button class="text-sm">See More</button>
+	<button>
+		<svg
+			class="mt-2"
+			xmlns="http://www.w3.org/2000/svg"
+			width="13"
+			height="9"
+			viewBox="0 0 13 9"
+			fill="none"
+		>
+			<path
+				fill-rule="evenodd"
+				clip-rule="evenodd"
+				d="M13 1.53636L11.4833 0L6.5 5.04805L1.51667 0L0 1.53636L6.5 8.12078L13 1.53636Z"
+				fill="#686B6F"
+			/>
+		</svg>
+	</button>
+	<div class="mx-2 mt-2 w-full border-t border-gray-300" />
+</div>
