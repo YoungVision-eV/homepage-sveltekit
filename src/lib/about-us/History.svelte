db7c0e0233870c30d8dd53fe4d36aa78b335d556
Jonas Bulik <jonas@bulik.dev>
2023-10-14 19:33:52 +0200

    Desktop design for history section

--- src/lib/about-us/History.svelte
+++ src/lib/about-us/History.svelte
@@ -2,15 +2,24 @@
 	import LunaPortrait from '$lib/assets/luna-about-us-history.jpeg';
 </script>
 
-<section class="w-full bg-light-green px-5 pt-10 pb-16 mb-28 rounded-2xl">
-	<h1 class="text-2xl font-bold font-serif">Die Geschichte</h1>
-	<img src={LunaPortrait} alt="" class="w-full mt-10 rounded-2xl object-cover h-96" />
-	<p class="mt-8">
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-		labore et dolore magna aliqua ut enim ad.
-	</p>
-	<p class="mt-8">
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-		labore et dolore magna aliqua ut enim ad.
-	</p>
+<section class="w-full mb-28">
+	<div class="md:flex gap-x-24 w-full h-full bg-light-green rounded-2xl px-5 md:px-20 pt-10 pb-16">
+		<h1 class="text-2xl md:hidden font-bold font-serif">Die Geschichte</h1>
+		<img
+			src={LunaPortrait}
+			alt=""
+			class="w-full mt-10 md:mt-0 rounded-2xl md:rounded-r-none object-cover h-96 md:h-[30rem] md:w-[29rem]"
+		/>
+		<div class="md:pt-12 md:pr-24">
+			<h1 class="hidden md:block text-4xl font-bold font-serif">Die Geschichte (Luna)</h1>
+			<p class="mt-8 md:mt-10">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+				labore et dolore magna aliqua ut enim ad.
+			</p>
+			<p class="mt-8">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+				labore et dolore magna aliqua ut enim ad.
+			</p>
+		</div>
+	</div>
 </section>
