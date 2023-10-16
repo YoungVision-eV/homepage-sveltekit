286523aaddc0ae5ba6ee20bd19b85327199c338d
Jonas Bulik <jonas@bulik.dev>
2023-10-16 07:31:39 +0200

    Desktop design for 'try this at home' section

--- src/lib/about-us/TryThisAtHome.svelte
+++ src/lib/about-us/TryThisAtHome.svelte
@@ -2,11 +2,16 @@
 	import Button from '$lib/components/Button.svelte';
 </script>
 
-<section class="w-full bg-yellow px-5 py-10">
-	<h1 class="text-2xl font-bold font-serif text-center">Try this with your friends!</h1>
-	<p class="text-sm mt-6">
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-		labore et dolore magna aliqua ut enim ad.
-	</p>
-	<Button class="mt-14 mx-auto" color="black" text="Call to action" />
+<section class="w-full bg-yellow px-5 md:pl-20 md:pr-40 py-10 md:pb-14 md:flex gap-x-28 items-end">
+	<div class="hidden md:block flex-shrink-0 w-60 h-60 bg-white" />
+	<div>
+		<h1 class="text-2xl md:text-4xl font-bold font-serif text-center md:text-left">
+			Try this with your friends!
+		</h1>
+		<p class="text-sm mt-6 md:mt-9">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua ut enim ad.
+		</p>
+		<Button class="mt-14 md:mt-7 mx-auto md:mx-0" color="black" text="Call to action" />
+	</div>
 </section>
