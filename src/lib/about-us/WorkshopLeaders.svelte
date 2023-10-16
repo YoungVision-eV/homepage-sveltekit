c5daa1540a1fba0bcedfbc20e87542561357cb92
Jonas Bulik <jonas@bulik.dev>
2023-10-16 07:40:56 +0200

    Desktop design for workshop leaders

--- src/lib/about-us/WorkshopLeaders.svelte
+++ src/lib/about-us/WorkshopLeaders.svelte
@@ -22,22 +22,30 @@
 	];
 </script>
 
-<section class="px-5 py-16">
-	<h1 class="text-2xl font-bold font-serif">{title}</h1>
-	<p class="mt-6">
+<section class="px-5 md:px-20 py-16 md:pt-44">
+	<h1 class="text-2xl md:text-4xl font-bold font-serif">{title}</h1>
+	<p class="mt-6 md:mt-9 md:w-1/2">
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
 		labore et dolore magna aliqua ut enim ad.
 	</p>
-	<div class="mt-10">
+	<div class="mt-10 md:mt-28 md:flex justify-between gap-y-24">
 		{#each leaders as leader}
-			<div class="mt-6 flex even:flex-row-reverse justify-between items-center">
+			<div class="leader mt-6 flex even:flex-row-reverse justify-between items-center">
 				<div class="bg-gray-300 rounded-full aspect-square h-52 w-auto" />
-				<div class="text-center">
-					<h3 class=" text-xl">{leader.name}</h3>
+				<div class="text-center md:mt-5">
+					<h3 class="text-xl">{leader.name}</h3>
 					<p class="mt-3 text-sm">{leader.job}</p>
 				</div>
 			</div>
 		{/each}
 	</div>
-	<ReadMoreExpandButton class="mt-12" />
+	<ReadMoreExpandButton class="mt-12 md:hidden" />
 </section>
+
+<style>
+	@media (min-width: 768px) {
+		.leader {
+			flex-direction: column !important; /* even selector has high priority */
+		}
+	}
+</style>
