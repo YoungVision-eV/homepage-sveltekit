431f5b5e5e89750976491f9108c0a22c125d1bee
Jonas Bulik <jonas@bulik.dev>
2023-10-26 13:56:01 +0200

    Add support-us hero

---
+++ src/lib/support-us/Hero.svelte
@@ -0,0 +1,9 @@
+<section class="w-full h-[34rem] bg-cover bg-center pt-10">
+	<h1 class="text-center text-3xl font-bold font-serif">Unterstütze Uns</h1>
+</section>
+
+<style>
+	section {
+		background-image: url($lib/assets/support-us-hero-background.jpeg);
+	}
+</style>
