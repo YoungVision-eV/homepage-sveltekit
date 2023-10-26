80cda88c6c6a3f80eba0fdaaf1f989b2d4dd7fa8
Jonas Bulik <jonas@bulik.dev>
2023-10-26 14:31:45 +0200

    Add become a supporting member section

---
+++ src/lib/support-us/BecomeSupportingMember.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Button from '$lib/components/Button.svelte';
+	import ReadMoreExpandButton from '$lib/components/ReadMoreExpandButton.svelte';
+</script>
+
+<section class="px-4 py-8 bg-light-green">
+	<h1 class="text-2xl font-serif font-bold">Werde Fördermitglied</h1>
+	<h2 class="mt-3 text-xl font-serif">
+		Die chance uns zu unterstutzen und mehr von uns zu erfahren!
+	</h2>
+	<p class="mt-8">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua ut enim ad. Lorem ipsum dolor sit amet, consectetur adipiscing
+		elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad.
+	</p>
+	<Button class="mt-8" color="black" text="Sign Up Now" />
+	<ReadMoreExpandButton class="mt-12" />
+</section>
--- src/routes/support-us/+page.svelte
+++ src/routes/support-us/+page.svelte
@@ -1,5 +1,7 @@
 <script>
+	import BecomeSupportingMember from '$lib/support-us/BecomeSupportingMember.svelte';
 	import Hero from '$lib/support-us/Hero.svelte';
 </script>
 
 <Hero />
+<BecomeSupportingMember />
