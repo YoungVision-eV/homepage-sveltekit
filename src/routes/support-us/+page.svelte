c1486c4e7aa5ef5a63dd3eaea524fa34514db0b7
Jonas Bulik <jonas@bulik.dev>
2023-10-26 15:02:30 +0200

    Implement donation methods component

--- src/routes/support-us/+page.svelte
+++ src/routes/support-us/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import BecomeSupportingMember from '$lib/support-us/BecomeSupportingMember.svelte';
 	import Hero from '$lib/support-us/Hero.svelte';
+	import DonationMethods from '$lib/support-us/OtherDonationPossiblities.svelte';
 </script>
 
 <Hero />
 <BecomeSupportingMember />
+<DonationMethods />
