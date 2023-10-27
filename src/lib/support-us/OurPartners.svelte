38aadbfb1e8daa31982d07841fe0c441467623ee
Jonas Bulik <jonas@bulik.dev>
2023-10-27 17:03:11 +0200

    Add support-us:partners section

---
+++ src/lib/support-us/OurPartners.svelte
@@ -0,0 +1,40 @@
+<script>
+	import ReadMoreExpandButton from '$lib/components/ReadMoreExpandButton.svelte';
+
+	const partners = [1, 2, 3, 4, 5];
+	const honours = new Array(12).fill({ name: 'Nachname, Vorname', place: 'Ort' });
+</script>
+
+<div class="hidden md:block bg-light-green px-20 py-14">
+	<section>
+		<h1 class="text-4xl font-serif font-bold">Unsere Partner</h1>
+		<ul class="flex justify-between mt-14">
+			{#each partners as partner}
+				<li>
+					<img
+						class="rounded-full w-40 h-40"
+						src={'https://placehold.co/400/D9D9D9/434343?text=Partner ' + partner}
+						alt="Partner"
+					/>
+				</li>
+			{/each}
+		</ul>
+	</section>
+	<section>
+		<h1 class="mt-32 font-serif font-bold text-4xl">YoungVision Dankes-Tafel</h1>
+		<p class="mt-5">
+			Hier möchten wir speziell Menschen danken die Fördermitglieder sind oder eine größere Summe
+			gespendet haben.
+		</p>
+		<!-- FIX: the right-most column should go all the way to the padding (right-aligned) -->
+		<ul class="mt-20 grid grid-cols-4 text-xl justify-between content-between w-full gap-y-14">
+			{#each honours as honour}
+				<li>
+					<h3 class="font-bold">{honour.name}</h3>
+					<p>{honour.place}</p>
+				</li>
+			{/each}
+		</ul>
+		<ReadMoreExpandButton class="mt-14" />
+	</section>
+</div>
--- src/routes/support-us/+page.svelte
+++ src/routes/support-us/+page.svelte
@@ -2,8 +2,10 @@
 	import BecomeSupportingMember from '$lib/support-us/BecomeSupportingMember.svelte';
 	import Hero from '$lib/support-us/Hero.svelte';
 	import DonationMethods from '$lib/support-us/DonationMethods.svelte';
+	import OurPartners from '$lib/support-us/OurPartners.svelte';
 </script>
 
 <Hero />
 <BecomeSupportingMember />
 <DonationMethods />
+<OurPartners />
