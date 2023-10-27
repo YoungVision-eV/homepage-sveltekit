0c7e040294b0092226bd400114ad9ad3f4acb8f0
Jonas Bulik <jonas@bulik.dev>
2023-10-27 13:31:24 +0200

    Implement desktop design for support-us:Become supporting member

--- src/lib/support-us/BecomeSupportingMember.svelte
+++ src/lib/support-us/BecomeSupportingMember.svelte
@@ -3,16 +3,16 @@
 	import ReadMoreExpandButton from '$lib/components/ReadMoreExpandButton.svelte';
 </script>
 
-<section class="px-4 py-8 bg-light-green">
-	<h1 class="text-2xl font-serif font-bold">Werde Fördermitglied</h1>
-	<h2 class="mt-3 text-xl font-serif">
+<section class="px-4 md:px-20 py-8 md:py-12 bg-light-green">
+	<h1 class="text-2xl md:text-4xl font-serif font-bold">Werde Fördermitglied</h1>
+	<h2 class="mt-3 md:mt-2 text-xl md:text-2xl font-serif">
 		Die chance uns zu unterstutzen und mehr von uns zu erfahren!
 	</h2>
-	<p class="mt-8">
+	<p class="mt-8 md:mt-10 md:text-xl">
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
 		labore et dolore magna aliqua ut enim ad. Lorem ipsum dolor sit amet, consectetur adipiscing
 		elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad.
 	</p>
-	<Button class="mt-8" color="black" text="Sign Up Now" />
-	<ReadMoreExpandButton class="mt-12" />
+	<Button class="mt-8 md:mt-6" color="black" text="Sign Up Now" />
+	<ReadMoreExpandButton class="mt-12 md:mt-8" />
 </section>
