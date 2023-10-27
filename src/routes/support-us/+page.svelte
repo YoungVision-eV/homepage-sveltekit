e1559795ceada1cb4a4891438bf42867bc9e7e4c
Jonas <mrlordalfred@gmail.com>
2023-10-27 09:50:45 +0200

    Fix donation method import

--- src/routes/support-us/+page.svelte
+++ src/routes/support-us/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import BecomeSupportingMember from '$lib/support-us/BecomeSupportingMember.svelte';
 	import Hero from '$lib/support-us/Hero.svelte';
-	import DonationMethods from '$lib/support-us/OtherDonationPossiblities.svelte';
+	import DonationMethods from '$lib/support-us/DonationMethods.svelte';
 </script>
 
 <Hero />
