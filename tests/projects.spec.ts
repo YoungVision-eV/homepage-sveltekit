36b5ad1c4b349cec895296362da18490e0edb186
Han2-Ro <hannes.rother@online.de>
2023-11-02 13:12:04 +0100

    added tests for filter with checkboxes

--- tests/projects.spec.ts
+++ tests/projects.spec.ts
@@ -6,3 +6,15 @@ test.beforeEach(async ({ page }) => {
 test('Projects page screenshot', async ({ page }) => {
 	await expect(page).toHaveScreenshot({ fullPage: true });
 });
+
+test('Filter members_only', async ({ page }) => {
+	await page.getByLabel("Nur Mitglieder").click();
+	await expect(page.getByRole("list").getByText("Nur Mitglieder")).not.toBeVisible();
+	await expect(page).toHaveScreenshot({ fullPage: true });
+});
+
+test('Filter for_all', async ({ page }) => {
+	await page.getByLabel("Für Alle").click();
+	await expect(page.getByRole("list").getByText("Für Alle")).not.toBeVisible();
+	await expect(page).toHaveScreenshot({ fullPage: true });
+});
