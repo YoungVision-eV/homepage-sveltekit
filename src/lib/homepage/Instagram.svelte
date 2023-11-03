a350eb795a51a4032e1cd55ec22bc426a1920a85
Jonas Bulik <jonas@bulik.dev>
2023-11-03 16:08:46 +0100

    Update index:instagram update

--- src/lib/homepage/Instagram.svelte
+++ src/lib/homepage/Instagram.svelte
@@ -1,19 +1,39 @@
 <script lang="ts">
-	// TODO: get from Instagram API
+	import ArrowRight from '$lib/assets/Icons/Arrow-Right.svg';
+	import ArrowLeft from '$lib/assets/Icons/Arrow-Left.svg';
 	import FirstImage from '$lib/assets/instagram/first.jpeg';
 	import SecondImage from '$lib/assets/instagram/second.jpeg';
 
+	// TODO: get from Instagram API
+
 	const images = [
 		{ src: FirstImage, alt: 'First Image' },
-		{ src: SecondImage, alt: 'Second Image' }
+		{ src: SecondImage, alt: 'Second Image' },
+		{ src: FirstImage, alt: 'First Image' },
+		{ src: SecondImage, alt: 'Second Image' },
+		{ src: FirstImage, alt: 'First Image' }
 	];
 </script>
 
-<section class="w-full pt-12 pb-20">
-	<h1 class="mx-3 text-3xl font-bold font-serif text-dark-green">Folge uns auf Instagram</h1>
-	<div class="mt-11 flex gap-x-2 min-w-full">
-		{#each images as image}
-			<img class="w-48 h-48 shrink-0 object-cover" src={image.src} alt={image.alt} />
-		{/each}
+<section class="w-full pt-12 pb-32 md:px-6">
+	<h1 class="mx-5 md:mx-12 text-3xl font-bold font-serif text-dark-green md:text-black">
+		Folge uns auf Instagram
+	</h1>
+	<div class="mt-11 relative">
+		<div class="flex gap-x-2 md:mx-14 max-w-full overflow-hidden">
+			{#each images as image}
+				<img class="w-52 h-52 shrink-0 object-cover" src={image.src} alt={image.alt} />
+			{/each}
+		</div>
+		<div class="absolute flex items-center justify-between top-20 w-full">
+			<button class="p-3">
+				<img src={ArrowLeft} alt="" class="w-6 h-6" />
+				<span class="sr-only">Previous Testimonial</span>
+			</button>
+			<button class="p-3">
+				<img src={ArrowRight} alt="" class="w-6 h-6" />
+				<span class="sr-only">Next Testimonial</span>
+			</button>
+		</div>
 	</div>
 </section>
