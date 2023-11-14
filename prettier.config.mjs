0c94427ffdfc1f3641e5463229833bd3710886cb
Jonas <jonas@bulik.dev>
2023-11-14 16:26:29 +0100

    Pin all package versions (#73)
    * Pin all package versions
    * Fix prettier config to align with editor config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import("prettier").Config} */
 const config = {
-	useTabs: true,
 	singleQuote: true,
 	trailingComma: 'none',
-	printWidth: 100,
 	plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
 	tailwindFunctions: ['clsx'],
 	overrides: [
