5e1bb137e451b9bc1d86ce58cc002c8c351398e1
Jonas Bulik <jonas@bulik.dev>
2023-11-14 17:26:35 +0100

    Add campfire image to end of members page
    See #57

--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import FooterBackground from '$lib/assets/members-footer-background.jpeg';
 	import Testimonial from '$lib/homepage/Testimonial.svelte';
 	import Hero from '$lib/members/Hero.svelte';
 	import MemberBenefits from '$lib/members/MemberBenefits.svelte';
@@ -7,3 +8,8 @@
 <Hero />
 <MemberBenefits />
 <Testimonial />
+<img
+	class="h-[35rem] w-full object-cover"
+	alt="YoungVision Menschen sitzen am Lagerfeuer"
+	src={FooterBackground}
+/>
