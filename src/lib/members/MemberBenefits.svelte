dd7c8fe60cb8efe90fb3598216aba2a903662d04
Jonas <jonas@bulik.dev>
2023-11-14 18:21:31 +0100

    57 add become member page (#74)
    * Add become members page
    * Change the spacing of the testimonials to make it work with the members page design as well
    
    See #57

---
+++ src/lib/members/MemberBenefits.svelte
@@ -0,0 +1,28 @@
+<main class="flex-col items-center px-4 pt-14 md:flex md:px-20 md:pt-36">
+	<img alt="YoungVision" class="h-48 w-full bg-gray-300 md:h-[27rem] md:w-[50rem]" />
+	<h2 class="mt-10 text-center font-serif text-2xl font-bold md:hidden">Werde Mitglied</h2>
+	<h2 class="mt-20 hidden text-center font-serif text-4xl font-bold md:block">
+		Was Kriegst du als Mitglied?
+	</h2>
+	<div class="mt-6 flex flex-col gap-4 md:mt-14 md:flex-row md:px-24">
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+			incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+			exercitation ullamco laboris.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+			incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+			exercitation ullamco laboris.
+		</p>
+	</div>
+
+	<div class="mt-10 flex w-full flex-col gap-4 md:mt-20 md:h-[22rem] md:flex-row">
+		<img alt="YoungVision" class="h-48 w-full bg-gray-300 md:h-full" />
+		<img alt="YoungVision" class="h-48 w-full bg-gray-300 md:h-full" />
+	</div>
+</main>
---
+++ src/routes/members/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import FooterBackground from '$lib/assets/members-footer-background.jpeg';
+	import Testimonial from '$lib/homepage/Testimonial.svelte';
+	import Hero from '$lib/members/Hero.svelte';
+	import MemberBenefits from '$lib/members/MemberBenefits.svelte';
+</script>
+
+<Hero />
+<MemberBenefits />
+<div class="md:py-24">
+	<Testimonial />
+</div>
+<img
+	class="h-[35rem] w-full object-cover md:h-[60rem]"
+	alt="YoungVision Menschen sitzen am Lagerfeuer"
+	src={FooterBackground}
+/>
+<footer class="h-60 bg-dark-green" />
