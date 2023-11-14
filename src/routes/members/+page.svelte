7b8b2ded7dcacfb59d2292c8bbce3013b6dd96d3
Jonas Bulik <jonas@bulik.dev>
2023-11-14 17:22:03 +0100

    Add testimonials to members page
    See #57

--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -1,7 +1,9 @@
 <script>
+	import Testimonial from '$lib/homepage/Testimonial.svelte';
 	import Hero from '$lib/members/Hero.svelte';
 	import MemberBenefits from '$lib/members/MemberBenefits.svelte';
 </script>
 
 <Hero />
 <MemberBenefits />
+<Testimonial />
