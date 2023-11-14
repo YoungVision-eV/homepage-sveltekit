d1e3cf13ed0fe8d194110c96c7c6ad49a5e2802d
Jonas Bulik <jonas@bulik.dev>
2023-11-14 17:18:58 +0100

    Add member benefit details
    See #57

---
+++ src/lib/members/MemberBenefits.svelte
@@ -0,0 +1,21 @@
+<main class="px-4 py-14">
+	<img alt="YoungVision" class="h-48 w-full bg-gray-300" />
+	<h2 class="mt-10 text-center font-serif text-2xl font-bold">Werde Mitglied</h2>
+	<p class="mt-6">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+		laboris. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+		incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+		ullamco laboris.
+	</p>
+	<p class="mt-4">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+		laboris. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+		incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+		ullamco laboris.
+	</p>
+
+	<img alt="YoungVision" class="mt-10 h-48 w-full bg-gray-300" />
+	<img alt="YoungVision" class="mt-4 h-48 w-full bg-gray-300" />
+</main>
--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import Hero from '$lib/members/Hero.svelte';
+	import MemberBenefits from '$lib/members/MemberBenefits.svelte';
 </script>
 
 <Hero />
+<MemberBenefits />
