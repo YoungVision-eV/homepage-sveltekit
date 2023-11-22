4a2cfa320f282b76f5a8b18014047025f18d69c7
Jonas Bulik <jonas@bulik.dev>
2023-11-22 23:59:49 +0100

    Move CTA below calendar on mobile

--- src/lib/homepage/Events.svelte
+++ src/lib/homepage/Events.svelte
@@ -3,24 +3,24 @@
 	import Calendar from './Calendar.svelte';
 </script>
 
-<section class="w-full bg-cover pb-10 pt-16">
+<section class="w-full bg-cover pt-16 md:pb-10">
 	<div class="px-5 md:px-16">
 		<h1 class="font-serif text-3xl font-bold md:text-4xl">Unsere Events</h1>
 	</div>
-	<div class="flex-col-reverse md:mt-14 md:flex">
-		<div class="px-5 pb-14 md:mt-14 md:px-16">
-			<div class="mt-4 justify-between gap-x-8 md:flex">
-				<p class="text-xl">
+	<div class="mt-6 md:mt-14">
+		<Calendar />
+		<div class="mt-10 px-5 md:mt-12 md:px-16">
+			<div class="justify-between gap-x-10 md:flex">
+				<p class="text-justify">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
 					ut labore et dolore magna aliqua.
 				</p>
-				<p class="hidden text-xl md:block">
+				<p class="hidden text-justify md:block">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
 					ut labore et dolore magna aliqua ut enim ad.
 				</p>
 			</div>
-			<Button class="mx-auto mt-14 md:mx-0 md:mt-8" text="Follow us on Telegram" color="black" />
+			<Button class="mx-auto mt-14 md:mx-0 md:mt-12" text="Follow us on Telegram" color="black" />
 		</div>
-		<Calendar />
 	</div>
 </section>
