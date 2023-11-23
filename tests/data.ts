9a89490708a70824d361b35fd7cc5379a1dae0da
Jonas Bulik <jonas@bulik.dev>
2023-11-23 19:09:49 +0100

    Rename Platform to Mitglieder in navbar

--- tests/data.ts
+++ tests/data.ts
@@ -2,6 +2,6 @@ export const SUB_PAGES = [
 	{ name: 'Home', url: '/' },
 	{ name: 'About', url: '/about-us' },
 	{ name: 'Support', url: '/support-us' },
-	{ name: 'Platform', url: '/members' }
+	{ name: 'Mitglieder', url: '/members' }
 	// TODO: Add tests for new sub pages
 ];
