2e839715e64bd9e66719f710df35d4e3ff16cb62
Han2-Ro <hannes.rother@online.de>
2023-11-23 19:59:05 +0100

    worked on desktop layout

--- src/lib/projects/YearlyEvents.svelte
+++ src/lib/projects/YearlyEvents.svelte
@@ -33,28 +33,32 @@
 	let only_members = true;
 </script>
 
-<section class="px-6 pt-14">
-	<h1 class="font-serif text-2xl font-bold">Our Yearly Events</h1>
+<section class="px-6 pt-14 lg:px-20 lg:pt-24">
+	<h2 class="font-serif text-2xl font-bold">Our Yearly Events</h2>
 	<p class="mt-5">
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
 		labore et dolore magna aliqua ut enim ad. Lorem ipsum dolor sit amet, consectetur adipiscing
 		elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad.
 	</p>
 	<div class="mt-3 flex flex-row">
-		<div class="flex flex-1 items-center">
+		<div class="flex items-center">
 			<input class="h-7 w-7" type="checkbox" id="for-all" bind:checked={for_all} />
 			<label class="ml-4" for="for-all">Für Alle</label>
 		</div>
-		<div class="flex flex-1 items-center">
+		<div class="ml-10 flex items-center lg:ml-28">
 			<input class="h-7 w-7" type="checkbox" id="only-members" bind:checked={only_members} />
 			<label class="ml-4" for="only-members">Nur Mitglieder</label>
 		</div>
 	</div>
-	<ul class="mt-12">
+	<ul class="mt-12 lg:grid lg:grid-cols-2 lg:gap-10">
 		{#each events as event}
 			{#if (event.for_all && for_all) || (!event.for_all && only_members)}
 				<li>
-					<img src={event.image} alt="" class="mt-12 rounded-t-2xl" />
+					<img
+						src={event.image}
+						alt=""
+						class="mt-12 rounded-t-2xl lg:h-64 lg:w-full lg:object-cover"
+					/>
 					<div
 						class="flex rounded-b-2xl p-6 shadow-lg {event.for_all
 							? 'bg-soft-yellow'
@@ -65,7 +69,7 @@
 							<div>{event.month}</div>
 						</div>
 						<div class="ml-10">
-							<h2 class="font-bold">{event.title}</h2>
+							<h3 class="font-bold lg:text-xl">{event.title}</h3>
 							<p class="mt-3">{event.text}</p>
 							<p class="mt-3 inline-block rounded-full bg-white px-5">
 								{event.for_all ? 'Für Alle' : 'Nur Mitglieder'}
