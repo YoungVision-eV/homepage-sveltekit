16a610a51c462915faeaf2a0ba5e88107b501fab
Jonas Bulik <jonas@bulik.dev>
2023-11-23 19:33:57 +0100

    Optimize image on members page
    See #45

--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import FooterBackground from '$lib/assets/members-footer-background.jpeg';
 	import Testimonial from '$lib/homepage/Testimonial.svelte';
 	import Hero from '$lib/members/Hero.svelte';
 	import MemberBenefits from '$lib/members/MemberBenefits.svelte';
@@ -10,8 +9,8 @@
 <div class="lg:py-24">
 	<Testimonial />
 </div>
-<img
+<enhanced:img
 	class="h-[35rem] w-full object-cover lg:h-[60rem]"
 	alt="YoungVision Menschen sitzen am Lagerfeuer"
-	src={FooterBackground}
+	src="$lib/assets/members-footer-background.jpeg"
 />
