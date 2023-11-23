2082d8143b981ce6873dc46ee6e0418081b32fc2
Jonas Bulik <jonas@bulik.dev>
2023-11-23 12:45:51 +0100

    Add more events

--- src/routes/events/[slug]/+page.ts
+++ src/routes/events/[slug]/+page.ts
@@ -8,6 +8,12 @@ type Event = {
 const events: Record<string, Event> = {
 	'summer-gathering-2024': {
 		title: 'Summer Gathering 2024'
+	},
+	'winter-gathering-2023': {
+		title: 'Winter Gathering 2023'
+	},
+	'mitglieder-versammlung-2023': {
+		title: 'Mitglieder Versammlung 2023'
 	}
 };
 
