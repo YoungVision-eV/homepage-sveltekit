b1fe8e6904fccc58913168171b753fe3576153c1
Jonas Bulik <jonas@bulik.dev>
2023-11-24 10:23:40 +0100

    Optimize image of Luna

--- src/lib/about-us/History.svelte
+++ src/lib/about-us/History.svelte
@@ -1,15 +1,13 @@
-<script>
-	import LunaPortrait from '$lib/assets/luna-about-us-history.jpeg';
-</script>
-
 <section class="mb-28 w-full">
 	<div class="h-full w-full gap-x-24 rounded-2xl bg-light-green px-5 pb-16 pt-10 lg:flex lg:px-20">
 		<h1 class="font-serif text-2xl font-bold lg:hidden">Die Geschichte</h1>
-		<img
-			src={LunaPortrait}
-			alt=""
-			class="mt-10 h-96 w-full rounded-2xl object-cover lg:mt-0 lg:h-[30rem] lg:w-[29rem] lg:rounded-r-none"
-		/>
+		<div class="flex-none">
+			<enhanced:img
+				src="$lib/assets/luna-about-us-history.jpeg"
+				alt=""
+				class="mt-10 h-96 w-full rounded-2xl object-cover lg:mt-0 lg:h-[30rem] lg:w-[29rem] lg:rounded-r-none"
+			/>
+		</div>
 		<div class="lg:pr-24 lg:pt-12">
 			<h1 class="hidden font-serif text-4xl font-bold lg:block">Die Geschichte (Luna)</h1>
 			<p class="mt-8 lg:mt-10">
