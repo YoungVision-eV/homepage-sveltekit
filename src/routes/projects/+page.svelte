f551661a7e56570e4391714f80b46762e9cfba0c
Jonas Bulik <jonas@bulik.dev>
2023-11-30 14:15:27 +0100

    Update projects screenshots

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,11 +3,9 @@
 	import YearlyEvents from '$lib/projects/YearlyEvents.svelte';
 	import Collaborations from '$lib/projects/Collaborations.svelte';
 	import NextEvents from '$lib/projects/NextEvents.svelte';
-	import Footer from '$lib/homepage/Footer.svelte';
 </script>
 
 <Hero />
 <NextEvents />
 <YearlyEvents />
 <Collaborations />
-<Footer />
