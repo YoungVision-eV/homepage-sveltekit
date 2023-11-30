58ac672cfd07a3d25d41eacdd0ffc193a4d548a0
Han2-Ro <hannes.rother@online.de>
2023-11-30 12:55:20 +0100

    added some background images

--- src/lib/projects/NextEvents.svelte
+++ src/lib/projects/NextEvents.svelte
@@ -1,9 +1,12 @@
 <script>
 	//Importing form hompage folder. Not sure if this is the way. Maybe Calender should be in lib/components/
 	import Calendar from '$lib/homepage/Calendar.svelte';
+	import background1 from '$lib/assets/projects-bg-lg-1.jpg';
+	import background2 from '$lib/assets/projects-bg-lg-2.jpg';
 </script>
 
-<section class="mt-14">
+<section class="relative mt-14">
+	<img src={background1} alt="" class="absolute -z-10 mt-16" />
 	<div class="mb-10 px-4 lg:mb-16 lg:px-20">
 		<h2 class="font-serif text-2xl font-bold">Our Next Events</h2>
 		<p class="mt-3 lg:mt-6">
@@ -11,5 +14,6 @@
 			labore et dolore magna aliqua.
 		</p>
 	</div>
+	<img src={background2} alt="" class="absolute -bottom-52 right-0 -z-20" />
 	<Calendar />
 </section>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,10 +3,11 @@
 	import YearlyEvents from '$lib/projects/YearlyEvents.svelte';
 	import Collaborations from '$lib/projects/Collaborations.svelte';
 	import NextEvents from '$lib/projects/NextEvents.svelte';
+	import Footer from '$lib/homepage/Footer.svelte';
 </script>
 
 <Hero />
 <NextEvents />
 <YearlyEvents />
 <Collaborations />
-<footer class="mt-14 h-64 w-full bg-dark-green" />
+<Footer />
