327f7ac30936560721e4ff654a392e6e4a0c7dd0
Han2-Ro <hannes.rother@online.de>
2023-12-01 16:45:48 +0100

    format

--- src/lib/data/events.ts
+++ src/lib/data/events.ts
@@ -3,49 +3,51 @@ import EventImage2 from '$lib/assets/projects-event-image-2.jpeg?enhanced';
 import EventImage3 from '$lib/assets/projects-event-image-3.jpeg?enhanced';
 
 type YVEvent = {
-    slug: string;
-    title: string;
-    day: string;
-    month: string;
-    short_description: string;
-    image: string;
-    for_all: boolean;
+	slug: string;
+	title: string;
+	day: string;
+	month: string;
+	short_description: string;
+	image: string;
+	for_all: boolean;
 };
 
 const events: YVEvent[] = [
-    {
-        slug: 'sommerveranstaltung',
-        title: 'Sommerveranstaltung',
-        day: '24',
-        month: 'August',
-        short_description: 'Unsere jährliche Sommerveranstaltung ist für viele das Highlight des Jahres!',
-        image: EventImage1,
-        for_all: true
-    },
-    {
-        slug: 'silvester',
-        title: 'Silvester',
-        day: '31',
-        month: 'Dezember',
-        short_description: 'Lass uns das vergangene Jahr ausklingen lassen und gemeinsam in das neue Jahr starten!',
-        image: EventImage2,
-        for_all: true
-    },
-    {
-        slug: 'mitgliederversammlung',
-        title: 'Mitgliederversammlung',
-        day: '25',
-        month: 'August',
-        short_description: 'Werde Mitglied und entscheide gemeinsam über die Zukunft von YoungVision!',
-        image: EventImage3,
-        for_all: false
-    }
+	{
+		slug: 'sommerveranstaltung',
+		title: 'Sommerveranstaltung',
+		day: '24',
+		month: 'August',
+		short_description:
+			'Unsere jährliche Sommerveranstaltung ist für viele das Highlight des Jahres!',
+		image: EventImage1,
+		for_all: true
+	},
+	{
+		slug: 'silvester',
+		title: 'Silvester',
+		day: '31',
+		month: 'Dezember',
+		short_description:
+			'Lass uns das vergangene Jahr ausklingen lassen und gemeinsam in das neue Jahr starten!',
+		image: EventImage2,
+		for_all: true
+	},
+	{
+		slug: 'mitgliederversammlung',
+		title: 'Mitgliederversammlung',
+		day: '25',
+		month: 'August',
+		short_description: 'Werde Mitglied und entscheide gemeinsam über die Zukunft von YoungVision!',
+		image: EventImage3,
+		for_all: false
+	}
 ];
 
 export function getEventBySlug(slug: string) {
-    return events.find((e) => e.slug === slug);
+	return events.find((e) => e.slug === slug);
 }
 
 export function getAllYearlyEvents() {
-    return events;
+	return events;
 }
