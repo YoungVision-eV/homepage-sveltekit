d6db5b156981f9591eeadd58dbd6bdadc52b100e
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2023-12-01 19:59:15 +0100

    Content for projects page (#126)
    * removed Collaboration with InKontakt section, because of missing content
    * implemented events.ts as source to get event data form
    * linked event pages
    * updated screenshots
    
    ---------
    
    Co-authored-by: Jonas <jonas@bulik.dev>

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,11 +1,9 @@
 <script>
 	import Hero from '$lib/projects/Hero.svelte';
 	import YearlyEvents from '$lib/projects/YearlyEvents.svelte';
-	import Collaborations from '$lib/projects/Collaborations.svelte';
 	import NextEvents from '$lib/projects/NextEvents.svelte';
 </script>
 
 <Hero />
 <NextEvents />
 <YearlyEvents />
-<Collaborations />
