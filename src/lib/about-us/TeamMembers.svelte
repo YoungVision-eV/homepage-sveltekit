60240605266f28acdaf51dcf780301840550520b
Jonas Bulik <jonas@bulik.dev>
2023-12-02 11:01:05 +0100

    New photos for the team members

--- src/lib/about-us/TeamMembers.svelte
+++ src/lib/about-us/TeamMembers.svelte
@@ -1,25 +1,29 @@
 <script lang="ts">
-	import placeholder from '$lib/assets/Placeholder.svg';
+	import EmmaPortrait from '$lib/assets/emma-portrait.png?enhanced';
+	import JakobPortrait from '$lib/assets/jakob-portait.jpeg?enhanced';
+	import RebeccaPortrait from '$lib/assets/rebecca-portrait.jpeg?enhanced';
+	import JonasPortrait from '$lib/assets/jonas-portrait.jpeg?enhanced';
+
 	const members = [
 		{
 			name: 'Emma',
 			job: 'Design, ÖA',
-			image: placeholder
+			image: EmmaPortrait
 		},
 		{
 			name: 'Jakob',
 			job: 'Finanzen, Events',
-			image: placeholder
+			image: JakobPortrait
 		},
 		{
 			name: 'Rebecca',
 			job: 'ÖA, Events',
-			image: placeholder
+			image: RebeccaPortrait
 		},
 		{
 			name: 'Jonas',
 			job: 'IT, Datenschutz',
-			image: placeholder
+			image: JonasPortrait
 		}
 	];
 </script>
@@ -29,7 +33,7 @@
 	<div class="mt-14 grid grid-cols-2 gap-x-5 gap-y-14 lg:grid-cols-4 lg:gap-x-24">
 		{#each members as member}
 			<div class="flex flex-col items-center">
-				<img
+				<enhanced:img
 					src={member.image}
 					alt={member.name}
 					class="aspect-square h-auto w-full rounded-full bg-white object-cover object-center"
