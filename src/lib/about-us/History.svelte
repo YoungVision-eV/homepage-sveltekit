eae6853e9e36e3f30b19d9d856cf0f42cdf33f67
Jonas Bulik <jonas@bulik.dev>
2023-12-04 10:25:36 +0100

    Replace luna with more general history

--- src/lib/about-us/History.svelte
+++ src/lib/about-us/History.svelte
@@ -1,15 +1,3 @@
-<script>
-	import { createCollapsible, melt } from '@melt-ui/svelte';
-	import { slide } from 'svelte/transition';
-	import ChevronUp from '$lib/icons/ChevronUp.svelte';
-	import ChevronDown from '$lib/icons/ChevronDown.svelte';
-
-	const {
-		elements: { root, content, trigger },
-		states: { open }
-	} = createCollapsible();
-</script>
-
 <section class="mb-28 px-4 lg:px-20">
 	<div
 		class="z-10 rounded-2xl bg-green-700 px-6 pb-10 pt-8 shadow-lg lg:flex lg:h-[30rem] lg:items-center lg:p-0"
@@ -17,51 +5,25 @@
 		<h2 class="font-serif text-2xl font-bold lg:hidden">YoungVision's Entstehung</h2>
 		<div class="mt-7 flex-none lg:mt-0">
 			<enhanced:img
-				src="$lib/assets/luna-about-us-history.jpeg"
+				src="$lib/assets/OG-youngvision-people.jpg"
 				alt=""
 				class="h-96 w-full rounded-2xl object-cover lg:h-[30rem] lg:w-[29rem] lg:rounded-r-none"
 			/>
 		</div>
 
-		<div use:melt={$root} class="rounded-2xl bg-green-700 py-4 lg:px-24">
+		<div class="rounded-2xl bg-green-700 py-4 lg:px-24">
 			<h2 class="hidden font-serif text-3xl font-bold lg:block">YoungVision's Entstehung</h2>
 			<p class="mt-4 lg:mt-6">
-				YoungVision wurde 2013 auf dem Celebrate Life Festival YoungVision gegründet, inspiriert von
-				den Ideen und Werten von
+				YoungVision wurde 2013, unter anderem inspiriert von Ideen und Werten von
 				<a class="text-blue-700 hover:underline" href="https://thomashuebl.com/">Thomas Hübl</a>.
 				Seitdem hat sich YoungVision stetig weiterentwickelt und ist zu einer engagierten
 				Gemeinschaft gewachsen.
 			</p>
 			<p class="mt-4">
-				Eine herausragende Persönlichkeit in der Entwicklung des Vereins war
-				<a class="text-blue-700 hover:underline" href="https://lunainkontakt.de/"
-					>Luna Garcia Fernandez</a
-				>, die mit unermüdlicher Energie, Leidenschaft und Hingabe nicht nur jährlich eine große
-				Sommerveranstaltung organisierte, sondern auch die Trainingsgruppen leitete.
+				Im Jahr 2023 hat sich mit einer demokratischen Organisationsstruktur und einem neuen
+				Vorstand der Fokus des Vereins verändert. YoungVision strebt nun danach, noch stärker zu
+				einer Gemeinschaft zu werden, die von ihren Mitgliedern aktiv mitgestaltet werden kann.
 			</p>
-			{#if $open}
-				<div class="mt-4" use:melt={$content} transition:slide>
-					<p>
-						Im Jahr 2023 hat sich mit einer demokratischen Organisationsstruktur und einem neuen
-						Vorstand der Fokus des Vereins verändert. YoungVision strebt nun danach, noch stärker zu
-						einer Gemeinschaft zu werden, die von ihren Mitgliedern aktiv mitgestaltet werden kann.
-					</p>
-				</div>
-			{/if}
-			<div class="z-10 mt-5 flex w-full flex-col items-center justify-center lg:mt-6">
-				<button use:melt={$trigger} class="flex items-center">
-					Mehr lesen
-					<div class="h-10 w-10 p-2">
-						{#if $open}
-							<ChevronUp />
-						{:else}
-							<ChevronDown />
-						{/if}
-					</div>
-				</button>
-
-				<div class="mx-2 mt-2 w-full border-t border-green-200" />
-			</div>
 		</div>
 	</div>
 </section>
