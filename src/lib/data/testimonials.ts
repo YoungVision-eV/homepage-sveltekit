270dbeac934dca1261aae62e08152e469570a25d
Jonas Bulik <jonas@bulik.dev>
2024-01-11 15:11:20 +0100

    Update screenshots

--- src/lib/data/testimonials.ts
+++ src/lib/data/testimonials.ts
@@ -12,7 +12,7 @@ const testimonials: Testimonial[] = [
 	{
 		name: 'Andrea',
 		age: 22,
-		text: 'Young Vision bedeutet für mich Verbundenheit mit mir selbst und anderen Leuten sowie Leichtigkeit und einfach einen Ort zu haben wo man für einen Moment mal den Alltagsstress beiseite lassen kann ❤️',
+		text: 'Young Vision bedeutet für mich Verbundenheit mit mir selbst und anderen Leuten sowie Leichtigkeit und einfach einen Ort zu haben wo man für einen Moment mal den Alltagsstress beiseite lassen kann',
 		image: AndreaProfile
 	},
 	{
