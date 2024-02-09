bbcdbb8dd8b820128e7a2021e359f5f4d25b6ea6
Jonas Bulik <jonas@bulik.dev>
2024-02-09 20:49:43 +0100

    Bauwoche:Add laptop design

--- src/routes/events/bauwoche-2024/+page.svelte
+++ src/routes/events/bauwoche-2024/+page.svelte
@@ -32,127 +32,143 @@
 	</div>
 </div>
 
-<div class="px-4 pb-24 pt-12">
-	<h1 class="font-serif text-4xl font-bold">Bauwoche</h1>
-	<h2 class="mt-6 font-serif text-2xl font-bold">Wichtige Infos</h2>
-	<div class="mt-16 grid grid-cols-2 gap-x-6 gap-y-14 px-4">
-		<div class="flex flex-col items-center">
-			<div class="flex h-40 w-40 items-center justify-center rounded-full bg-yellow-700 shadow-xl">
-				<LocationPin class="h-24 w-24" />
+<div class="px-4 pt-12 lg:px-20 lg:text-xl">
+	<h1 class="font-serif text-4xl font-bold lg:text-5xl">Bauwoche</h1>
+	<div class="mt-6 lg:mt-14 lg:px-24">
+		<h2 class="font-serif text-2xl font-bold lg:text-4xl">Wichtige Infos</h2>
+		<div class="mt-16 grid grid-cols-2 gap-x-6 gap-y-14 px-4 lg:grid-cols-4 lg:px-0">
+			<div class="flex flex-col items-center">
+				<div
+					class="flex h-40 w-40 items-center justify-center rounded-full bg-yellow-700 shadow-xl"
+				>
+					<LocationPin class="h-24 w-24" />
+				</div>
+				<h3 class="mt-8 font-serif text-xl font-bold">Wo</h3>
+				<a class="mt-3 underline" href="https://maps.app.goo.gl/kak1RX8pJwZGnFFD7">
+					<address class="not-italic">Am bhf 4, 166307 Mescherin</address>
+				</a>
 			</div>
-			<h3 class="mt-8 font-serif text-xl font-bold">Wo</h3>
-			<a class="mt-3 underline" href="https://maps.app.goo.gl/kak1RX8pJwZGnFFD7">
-				<address class="not-italic">Am bhf 4, 166307 Mescherin</address>
-			</a>
+
+			<div class="flex flex-col items-center">
+				<div
+					class="flex h-40 w-40 items-center justify-center rounded-full bg-yellow-700 shadow-xl"
+				>
+					<People class="h-24 w-24" />
+				</div>
+				<h3 class="mt-8 font-serif text-xl font-bold">Wer</h3>
+				<p class="mt-3">20 junge Menschen</p>
+			</div>
+			<div class="flex flex-col items-center">
+				<div
+					class="flex h-40 w-40 items-center justify-center rounded-full bg-yellow-700 shadow-xl"
+				>
+					<CalendarIcon class="h-24 w-24" />
+				</div>
+				<h3 class="mt-8 font-serif text-xl font-bold">Wann</h3>
+				<p class="mt-3">01.04. - 07.04.2024</p>
+			</div>
+			<div class="flex flex-col items-center">
+				<div
+					class="flex h-40 w-40 items-center justify-center rounded-full bg-yellow-700 shadow-xl"
+				>
+					<HandWithMoney class="h-24 w-24" />
+				</div>
+				<h3 class="mt-8 font-serif text-xl font-bold">Kosten</h3>
+				<p class="mt-3">Verpflegungskosten sind 50 Euro.</p>
+			</div>
+		</div>
+		<div class="mt-20 lg:mt-28">
+			<h2 class="font-serif text-2xl font-bold lg:text-4xl">Brief an Euch</h2>
+			<p class="mt-10 lg:mt-12">Hallo du lieber Mensch🧚🏻‍♂️✨,</p>
+			<p class="mt-4">
+				vom <b>1. bis 7. April</b> laden wir dich ein zu einer Woche voller
+				<b>Gemeinschaft, Kreativität und gemeinsamen Schaffens.</b> Zusammen mit etwa
+				<b>20 jungen Menschen</b> werden wir unseren <b>Vereinssitz</b> für die kommende Veranstaltungssaison
+				fit machen.
+			</p>
+			<p class="mt-4">
+				In der Woche werden wir eine <b>Komposttoilette</b> bauen, das <b>YV Lager</b> einrichten
+				und eine <b>Plattform</b>
+				für unsere Veranstaltungszelt bauen. Wir werden täglich mit einer <b>Morgenrunde</b>
+				beginnen, um uns zu verbinden und den Tag gemeinsam zu starten. Abends haben wir Zeit für
+				<b>Spiele, Lagerfeuer und Massagen.</b>
+				Du hast Lust einen Programmpunkt anzubieten? Dann schreib uns gerne eine Email an
+				<a class="text-gray-800 underline" href="mailto:jakob.voigt@youngvision.org"
+					>jakob.voigt@youngvision.org</a
+				>.
+			</p>
+			<p class="mt-4">
+				Mit deinem Betrag von 50€ können wir einen Teil deiner Verpflegungskosten decken, die
+				Übernachtung ist für dich kostenfrei. Wenn du dir den Beitrag gerade nicht leisten kannst,
+				schreibe uns richtig gerne eine Email.
+			</p>
+			<p class="mt-4">
+				Um die Bauwoche bestmöglich vorzubereiten und uns schon vorab kennenzulernen, laden wir dich
+				herzlich zu unserem Online-Treffen am 13. März 2024 von 17:30 bis 19:00 Uhr ein (optional).
+			</p>
+			<p class="mt-4">
+				Bis ganz Bald💜,<br />
+				Euer YV Team
+			</p>
+
+			<Button
+				text="Anmelden"
+				color="dark"
+				class="mt-12 lg:mt-8"
+				href="https://coda.io/form/YoungVision-Bauwoche-2024_dfqXbo6awYm"
+			/>
 		</div>
 
-		<div class="flex flex-col items-center">
-			<div class="flex h-40 w-40 items-center justify-center rounded-full bg-yellow-700 shadow-xl">
-				<People class="h-24 w-24" />
+		<div class="mt-32 lg:mt-28">
+			<h2 class="font-serif text-2xl font-bold lg:text-4xl">Unsere Bauprojekte</h2>
+
+			<div class="mt-14 flex flex-col items-center gap-12 lg:flex-row lg:px-10">
+				<div>
+					<KompostToiletteIcon />
+					<h3 class="mt-4 text-xl font-bold">Komposttoilette</h3>
+				</div>
+				<div>
+					<LagerIcon />
+					<h3 class="mt-4 text-xl font-bold">Lager</h3>
+				</div>
+				<div>
+					<PlattformIcon />
+					<h3 class="mt-4 text-xl font-bold">Plattform</h3>
+				</div>
 			</div>
-			<h3 class="mt-8 font-serif text-xl font-bold">Wer</h3>
-			<p class="mt-3">20 junge Menschen</p>
 		</div>
-		<div class="flex flex-col items-center">
-			<div class="flex h-40 w-40 items-center justify-center rounded-full bg-yellow-700 shadow-xl">
-				<CalendarIcon class="h-24 w-24" />
-			</div>
-			<h3 class="mt-8 font-serif text-xl font-bold">Wann</h3>
-			<p class="mt-3">01.04. - 07.04.2024</p>
-		</div>
-		<div class="flex flex-col items-center">
-			<div class="flex h-40 w-40 items-center justify-center rounded-full bg-yellow-700 shadow-xl">
-				<HandWithMoney class="h-24 w-24" />
-			</div>
-			<h3 class="mt-8 font-serif text-xl font-bold">Kosten</h3>
-			<p class="mt-3">Verpflegungskosten sind 50 Euro.</p>
+
+		<div class="mt-24 lg:mt-28">
+			<h2 class="font-serif text-2xl font-bold lg:text-4xl">Wochenplan</h2>
+
+			<Button
+				text="Wochenplan runterladen"
+				href="/Bauwoche-Wochenplan.png"
+				download="YoungVision Bauwoche 2024 - Wochenplan"
+				color="dark"
+				class="mt-8 lg:hidden"
+			/>
 		</div>
 	</div>
-	<div class="mt-20">
-		<h2 class="font-serif text-2xl font-bold">Brief an Euch</h2>
-		<p class="mt-10">Hallo du lieber Mensch🧚🏻‍♂️✨,</p>
-		<p class="mt-4">
-			vom <b>1. bis 7. April</b> laden wir dich ein zu einer Woche voller
-			<b>Gemeinschaft, Kreativität und gemeinsamen Schaffens.</b> Zusammen mit etwa
-			<b>20 jungen Menschen</b> werden wir unseren <b>Vereinssitz</b> für die kommende Veranstaltungssaison
-			fit machen.
-		</p>
-		<p class="mt-4">
-			In der Woche werden wir eine <b>Komposttoilette</b> bauen, das <b>YV Lager</b> einrichten und
-			eine <b>Plattform</b>
-			für unsere Veranstaltungszelt bauen. Wir werden täglich mit einer <b>Morgenrunde</b> beginnen,
-			um uns zu verbinden und den Tag gemeinsam zu starten. Abends haben wir Zeit für
-			<b>Spiele, Lagerfeuer und Massagen.</b>
-			Du hast Lust einen Programmpunkt anzubieten? Dann schreib uns gerne eine Email an
-			<a class="text-gray-800 underline" href="mailto:jakob.voigt@youngvision.org"
-				>jakob.voigt@youngvision.org</a
-			>.
-		</p>
-		<p class="mt-4">
-			Mit deinem Betrag von 50€ können wir einen Teil deiner Verpflegungskosten decken, die
-			Übernachtung ist für dich kostenfrei. Wenn du dir den Beitrag gerade nicht leisten kannst,
-			schreibe uns richtig gerne eine Email.
-		</p>
-		<p class="mt-4">
-			Um die Bauwoche bestmöglich vorzubereiten und uns schon vorab kennenzulernen, laden wir dich
-			herzlich zu unserem Online-Treffen am 13. März 2024 von 17:30 bis 19:00 Uhr ein (optional).
-		</p>
-		<p class="mt-4">
-			Bis ganz Bald💜,<br />
-			Euer YV Team
-		</p>
-
-		<Button
-			text="Anmelden"
-			color="dark"
-			class="mt-12 lg:mt-14"
-			href="https://coda.io/form/YoungVision-Bauwoche-2024_dfqXbo6awYm"
-		/>
-	</div>
-
-	<div class="mt-32">
-		<h2 class="font-serif text-2xl font-bold">Unsere Bauprojekte</h2>
-
-		<div class="mt-14 flex flex-col items-center gap-y-12">
-			<div>
-				<KompostToiletteIcon />
-				<h3 class="mt-4 text-xl font-bold">Komposttoilette</h3>
-			</div>
-			<div>
-				<LagerIcon />
-				<h3 class="mt-4 text-xl font-bold">Lager</h3>
-			</div>
-			<div>
-				<PlattformIcon />
-				<h3 class="mt-4 text-xl font-bold">Plattform</h3>
-			</div>
-		</div>
-	</div>
-
-	<div class="mt-24">
-		<h2 class="font-serif text-2xl font-bold">Wochenplan</h2>
-
-		<Button
-			text="Wochenplan runterladen"
-			href="/Bauwoche-Wochenplan.png"
-			download="YoungVision Bauwoche 2024 - Wochenplan"
-			color="dark"
-			class="mt-8"
-		/>
-	</div>
-
-	<div class="mt-24 flex flex-col items-center">
-		<div class="h-64 w-64 rounded-full">
+</div>
+<div class="mt-20 hidden lg:block">
+	<enhanced:img src="$lib/assets/Bauwoche-Wochenplan.png" alt="Wochenplan Bauwoche 2024" />
+</div>
+<div class="py-24 lg:px-44 lg:py-28 lg:text-xl">
+	<div class="flex flex-col items-center lg:flex-row lg:gap-x-20">
+		<div class="h-64 w-64 rounded-full lg:h-80 lg:w-80">
 			<enhanced:img
 				class="h-full w-full rounded-full object-cover object-center"
 				src="$lib/assets/jakob-und-jonas.png"
 				alt="Jakob & Jonas"
 			/>
 		</div>
-		<p class="mt-6 text-xl font-bold">Wir freuen uns auf dich!</p>
-		<p class="mt-4">Links: Jonas Roth (Handwerk)</p>
-		<p>Rechts: Jakob Voigt (Organisation)</p>
+		<div class="text-center">
+			<p class="mt-6 font-serif text-xl font-bold lg:text-3xl">Wir freuen uns auf dich!</p>
+			<p class="mt-4">Links: Jonas Roth (Handwerk)</p>
+			<p>Rechts: Jakob Voigt (Organisation)</p>
 
-		<Button text="Anmelden" color="dark" class="mt-12 lg:mt-14" />
+			<Button text="Anmelden" color="dark" class="mt-12 lg:mt-14" />
+		</div>
 	</div>
 </div>
