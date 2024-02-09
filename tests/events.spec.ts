38827c98269aa1fb2bfb4d6cc33ad1b7dd9a09ab
Jonas Bulik <jonas@bulik.dev>
2024-02-09 20:07:56 +0100

    Add bauwoche mobile tests

--- tests/events.spec.ts
+++ tests/events.spec.ts
@@ -4,3 +4,8 @@ test('Summer Gathering 2024 screenshot', async ({ page }) => {
 	await page.goto('/events/summer-gathering-2024');
 	await expect(page).toHaveScreenshot({ fullPage: true });
 });
+
+test('Bauwoche 2024 screenshot', async ({ page }) => {
+	await page.goto('/events/bauwoche-2024');
+	await expect(page).toHaveScreenshot({ fullPage: true });
+});
